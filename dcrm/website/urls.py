87124d0f5c914a5e5c81b52dcc85429914057df6
refraincr <zxczxc9861@qq.com>
2025-01-23 15:54:12 +0800

    registration

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 
 def home(request):
-    return  render(request,'website/home.html',{})
+    # return  render(request,'website/home.html',{})
+    if request.method == "POST":
+        username  = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials...")
+            return redirect('home')
+    else:
+        return render(request,'website/home.html',{})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You are now logged out...")
+    return redirect('home')
+
+def register_view(request):
+    return render(request,'website/register.html',{})
